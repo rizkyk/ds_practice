3c2e8baf176cccd8722acd5edd8fb8367eeed9f0
neomicrokid <neomicrokid@gmail.com>
2015-11-12 18:25:01 +0700

    add poisson distribution

---
+++ r_tut_stat/ESR/Probability_Distribution/binomial_dist.R
@@ -0,0 +1,34 @@
+### Binomial Distribution : discrete probability distribution
+### ========================
+
+## Problem
+# Suppose there are 12 multiple choise questions in an english quiz.
+# Each question has five possible answer, and only one of them is correct. 
+# Find the probability of having four or less correct answer 
+# if a student attempts to answer every question at random.
+
+## Solution
+# since only one out of five possibe answer is correct, the probability of answering 
+# a question correctly by random is 1/5 = 0.2 
+
+rst <- dbinom(4, size = 12, prob = 0.2)
+print(rst)
+
+# Find the probability of having exactly 4 correct answer by random attempt as follow
+
+xout = 0
+xx <- function(x) {
+  dbinom(x, size = 12, prob = 0.2)
+  
+  #xout <- xout + y  
+}
+
+# returning vector
+bino <-  sapply(c(0:4), xx)
+# or using lapply to return as list then unlist to revert to vector aim to sum values
+#bino <- unlist(lapply(c(0:4), xx))
+bino <- sum( bino)
+print(bino)
+
+## Answer 
+# variance of the eruption duration is 1.302728
